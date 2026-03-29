4c089a0be27ec9cd1fb78d18f441c677f0fcf3b5
MokshgnaIsm <gorantlamokshgna980@gmail.com>
2026-03-29 20:22:51 +0530

    reg / login / identify functionality added

--- backend/models/groupChat.js
+++ backend/models/groupChat.js
@@ -8,7 +8,6 @@ const groupChatSchema = new mongoose.Schema({
     group_contents: [{type:String}],
     messages: [
             {
-                message_id : {type:String,required:true},
                 sender_id : {type:String,required:true},
                 content : {type:String},
                 fetchables : [{type:String}],
